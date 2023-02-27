77b67e32def8a0278ecdb407eab37ea3f6a3d138
David Pérez <dperezsache.guadalupe@alumnado.fundacionloyola.net>
2023-02-27 09:34:58 +0100

    Reto 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { FavoritasComponent } from './favoritas/favoritas.component';
+import { CreditosComponent } from './creditos/creditos.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    PeliculasComponent,
+    FavoritasComponent,
+    CreditosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
